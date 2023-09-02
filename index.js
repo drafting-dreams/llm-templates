61e6638223460aaa7953dc5368b1bac5189d6f54
shawn_white <draftingdreams666@gmail.com>
2023-09-02 17:59:05 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,3 @@
+const explainTheDifferenceBetweenTwoTerms = `Explain the difference between ${term1} and ${term2}`
+const grammarExplainer = `Analyze the grammatical points of the following sentence by splitting it to smaller parts. Your answer should be as detailed as possible.
+The sentence: ${sentense}`
