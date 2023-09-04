64236aadf0e3180d5bd8faff6a95fae3dbc9e71b
liuxiaoyang <xiaoyang.liu@shopee.com>
2023-09-04 14:19:39 +0800

    feat: native translator

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const explainTheDifferenceBetweenTwoTerms = `Explain the difference between ${term1} and ${term2}`
 const grammarExplainer = `Analyze the grammatical points of the following sentence by splitting it to smaller parts. Your answer should be as detailed as possible.
-The sentence: ${sentense}`
+The sentence: ${sentence}`
+const nativeTranslator = `You're an American native. And you're good at translating between English and Chinese. Please translate the following sentence. If it's in English, translate it into Chinese. If it's in Chinese, translate it into English. You are free to use any slangs. And you should only return the translation without any other texts.
+The sentence: ${sentence}`
