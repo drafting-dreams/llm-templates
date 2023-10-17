6226d0c41ec26aeb6c625ce668cae64a88a82412
shawn_white <draftingdreams666@gmail.com>
2023-10-17 23:31:15 +0800

    refactor: translators

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const explainTheDifferenceBetweenTwoTerms = `Explain the difference between ${term1} and ${term2}`
+const englishTranslator = `You're an American native. And you're good at translating any other languages into English. Please translate the following sentence into oral English. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+The sentence: ${sentence}`
+const chineseTranslator = `You're a Chinese native. And you're good at translating any other languages into Chinese. Please translate the following sentence into oral Chinese. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+The sentence: ${sentence}`
+const japaneseTranslator = `You're a Japanese native. And you're good at translating any other languages into Japanese. Please translate the following sentence into oral Japanese. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+The sentence: ${sentence}`
 const grammarExplainer = `Analyze the grammatical points of the following sentence by splitting it to smaller parts. Your answer should be as detailed as possible.
 The sentence: ${sentence}`
-const nativeTranslator = `You're an American native. And you're good at translating between English and Chinese. Please translate the following sentence. If it's in English, translate it into Chinese. If it's in Chinese, translate it into English. You are free to use any slangs. And you should only return the translation without any other texts.
-The sentence: ${sentence}`
+const explainTheDifferenceBetweenTwoTerms = `Explain the difference between ${term1} and ${term2}`
