835f059056dfa0cd401c22b28decb258a7a1cb47
liuxiaoyang <xiaoyang.liu@shopee.com>
2024-02-14 17:10:00 +0800

    feat: tweak translators

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const englishTranslator = `You're an American native. And you're good at translating any other languages into English. Please translate the following sentence into oral English. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+const englishTranslator = `You're an American native. And you're good at translating any other languages into English. Please translate the following sentence into oral English. You can use slangs in your translation, but only use them when necessary, don't overuse them. And you should only return the translation without any other extra texts.
 The sentence: ${sentence}`
-const chineseTranslator = `You're a Chinese native. And you're good at translating any other languages into Chinese. Please translate the following sentence into oral Chinese. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+const chineseTranslator = `You're a Chinese native. And you're good at translating any other languages into Chinese. Please translate the following sentence into oral Chinese. You can use slangs in your translation, but only use them when necessary, don't overuse them. And you should only return the translation without any other extra texts.
 The sentence: ${sentence}`
-const japaneseTranslator = `You're a Japanese native. And you're good at translating any other languages into Japanese. Please translate the following sentence into oral Japanese. You are free to use any slangs to make your translation sound like from a native. And you should only return the translation without any other extra texts.
+const japaneseTranslator = `You're a Japanese native. And you're good at translating any other languages into Japanese. Please translate the following sentence into oral Japanese. You can use slangs in your translation, but only use them when necessary, don't overuse them. And you should only return the translation without any other extra texts.
 The sentence: ${sentence}`
 const grammarExplainer = `Analyze the grammatical points of the following sentence by splitting it to smaller parts. Your answer should be as detailed as possible.
 The sentence: ${sentence}`
