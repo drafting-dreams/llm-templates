fe1b26d2dd0b56e3f508b1e72b22adc14a33c1f2
liuxiaoyang <xiaoyang.liu@shopee.com>
2024-10-04 18:16:47 +0800

    feat: self assessment

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ The sentence: ${sentence}`
 const grammarExplainer = `Analyze the grammatical points of the following sentence by splitting it to smaller parts. Your answer should be as detailed as possible.
 The sentence: ${sentence}`
 const explainTheDifferenceBetweenTwoTerms = `Explain the difference between ${term1} and ${term2}`
+const selfAssessment = `Help me write Self Assessment for Year End Appraisal.
+
+My Job: Senior FrontEnd Engineer
+Performance Dimension: ${dimension}
+Dimension Description: ${dimension_description}
+Average Guideline of the Dimension: ${average_guideline}
+Outstanding Guideline of the Dimension: ${outstanding_guideline}
+The work I've done: ${my_work}
+
+Write the the self assessment for the performance dimension mentioned above as if you were me. You need to try your best reflect the description of the dimension and make the one who reads the assessment want to give me an "outstanding" score for the dimension based on the work I've done.
+The assessment can't be too long, it should be within 3 paragraphs and 250 words.
+`
